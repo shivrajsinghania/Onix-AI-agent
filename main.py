f19f14617fe53010960659b09e987e00648a1ad3
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-05-26 17:58:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,200 @@
-import json
-
 from flask import Flask, request, jsonify, render_template
-
+from datetime import datetime
 from tools.ai import ask_ai
 from tools.whatsapp import send_message
-from tools.db_history import save_history
 from tools.search import search
 from tools.validator import validate_task
-from tools.queue_manager import (add_task, get_pending_tasks, mark_completed, mark_running, mark_failed, retry_task)
-from tools.workflow_manager import (create_workflow, update_step, complete_workflow)
-from tools.context_manager import (store_step_output, get_step_output)
 from tools.observer import observe_website
 from tools.browser_actions import (open_website, click_element, type_text, submit_form)
 from config.database import get_connection
-from config.database import init_db
+from config.database import create_tables
 
 app = Flask(__name__)
-init_db()
+
+create_tables()
+
+def save_history(task, status):
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+
+    INSERT INTO history (
+        action,
+        app,
+        target,
+        message,
+        query,
+        status
+    )
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """, (
+        task.get("action"),
+        task.get("app"),
+        task.get("target"),
+        task.get("message"),
+        task.get("query"),
+        status
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def create_workflow(tasks):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO workflows (
+        workflow_json,
+        status,
+        current_step,
+        started_at
+    )
+    VALUES (%s, %s, %s, %s)
+    RETURNING id
+    """, (
+
+        json.dumps(tasks),
+        "running",
+        0,
+        datetime.now()
+    ))
+
+    workflow_id = cursor.fetchone()[0]
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return workflow_id
+
+def update_workflow_step(workflow_id, step):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE workflows
+    SET current_step=%s
+    WHERE id=%s
+    """, (
+        step,
+        workflow_id
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def complete_workflow(workflow_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE workflows
+    SET
+        status=%s,
+        completed_at=%s
+    WHERE id=%s
+    """, (
+        "completed",
+        datetime.now(),
+        workflow_id
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def add_task(task):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO task_queue (
+        task_json,
+        status,
+        attempts
+    )
+    VALUES (%s, %s, %s)
+    RETURNING id
+    """, (
+        json.dumps(task),
+        "pending",
+        0
+    ))
+    task_id = cursor.fetchone()[0]
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+    return task_id
+
+def mark_running(task_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE task_queue
+    SET
+        status=%s,
+        started_at=%s
+    WHERE id=%s
+    """, (
+        "running",
+        datetime.now(),
+        task_id
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def mark_completed(task_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE task_queue
+    SET
+        status=%s,
+        completed_at=%s
+    WHERE id=%s
+    """, (
+        "completed",
+        datetime.now(),
+        task_id
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def mark_failed(task_id, error):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE task_queue
+    SET
+        status=%s,
+        error=%s,
+        attempts=attempts+1
+    WHERE id=%s
+    """, (
+        "failed",
+        str(error),
+        task_id
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 
 
 @app.route("/")
@@ -28,27 +206,21 @@ def home():
 def ask():
 
     user_data = request.get_json()
-
     question = user_data["message"]
-
     response = ask_ai(question)
 
     try:
 
         workflow_data = json.loads(response)
         tasks = workflow_data["workflow"]
-        workflow = create_workflow(tasks)
+        workflow_id = create_workflow(tasks)
         
-        for step, task in enumerate(workflow["tasks"]):
-        	update_step(workflow["id"], step)
+        for step, task in enumerate(tasks):
+        	update_workflow_step(workflow_id, step)
         	task["step"] = step
         	add_task(task)
         
-        pending_tasks = get_pending_tasks()
-        for index, item in pending_tasks:
-        	
-        	task = item["task"]
-        	step = task["step"]
+        for step, task in enumerate(tasks):
         	action = task["action"]
         	
         	is_valid, reason = validate_task(task)
@@ -56,52 +228,45 @@ def ask():
         		print(f"Task rejected: {reason}")
         		continue
         	try:
-        		mark_running(index)
+        		task_id = add_task(task)
+        		mark_running(task_id)
         		
         		if action == "send_message":
         			target = task["target"]
         			message = task["message"]
         			send_message(target, message)
-        			store_step_output(workflow["id"], step, f"sent message to {target}" )
         			
         		elif action == "search":
         			app_name = task["app"]
         			query = task["query"]
         			search(app_name, query)
-        			store_step_output(workflow["id"], step, f"Searched {query} on {app_name}" )
         		
         		elif action == "observe_website":
         			url = task["url"]
         			observation = observe_website(url)
         			print(observation)
-        			store_step_output(workflow["id"], step, observation)
         			
         		elif action == "open_website":
         			url = task["url"]
         			open_website(url)
-        			store_step_output(workflow["id"], step, f"Opened {url}")
         			
         		elif action == "click_element":
         			element = task["element"]
         			click_element(element)
-        			store_step_output(workflow["id"], step, f"Clicked {element}")
         			
         		elif action == "type_text":
         			text = task["text"]
         			type_text(text)
-        			store_step_output(workflow["id"], step, f"Typed {text}")
         			
         		elif action == "submit_form":
         			submit_form()
-        			store_step_output(workflow["id"], step, "Submitted form")
         		
         		save_history(task)
-        		mark_completed(index)
-        		complete_workflow(workflow["id"])
+        		mark_completed(task_id)
+        		complete_workflow(workflow_id)
         	
         	except Exception as e:
-        		mark_failed(index, e)
-        		retry_task(index)
+        		mark_failed(task_id, e)
         		print(f"Execution failed: {e}")
         		
         return jsonify({
@@ -126,27 +291,18 @@ def history():
     cursor = conn.cursor()
 
     cursor.execute("""
-
     SELECT *
-
     FROM history
-
     ORDER BY id DESC
-
     """)
 
     rows = cursor.fetchall()
-
     cursor.close()
-
     conn.close()
-
     data = []
 
     for row in rows:
-
         data.append({
-
             "id": row[0],
             "action": row[1],
             "app": row[2],
@@ -154,40 +310,29 @@ def history():
             "message": row[4],
             "query": row[5],
             "created_at": str(row[6])
-
         })
 
     return jsonify(data)
 	
 @app.route("/queue")
 def queue():
-
     conn = get_connection()
-
     cursor = conn.cursor()
 
     cursor.execute("""
-
     SELECT *
-
     FROM task_queue
-
     ORDER BY id DESC
-
     """)
 
     rows = cursor.fetchall()
 
     cursor.close()
-
     conn.close()
-
     data = []
 
     for row in rows:
-
         data.append({
-
             "id": row[0],
             "task": row[1],
             "status": row[2],
@@ -196,49 +341,36 @@ def queue():
             "started_at": str(row[5]),
             "completed_at": str(row[6]),
             "created_at": str(row[7])
-
         })
 
     return jsonify(data)
 	
 @app.route("/workflows")
 def workflows():
-
     conn = get_connection()
-
     cursor = conn.cursor()
 
     cursor.execute("""
-
     SELECT *
-
     FROM workflows
-
     ORDER BY id DESC
-
     """)
 
     rows = cursor.fetchall()
 
     cursor.close()
-
     conn.close()
-
     data = []
 
     for row in rows:
-
         data.append({
-
             "id": row[0],
             "workflow": row[1],
             "status": row[2],
             "current_step": row[3],
             "started_at": str(row[4]),
             "completed_at": str(row[5]),
-            "duration": row[6],
             "created_at": str(row[7])
-
         })
 
     return jsonify(data)
