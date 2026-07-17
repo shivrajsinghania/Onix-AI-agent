544b571f288cb6660f7cc26feca4f87a06ccf043
Shivraj singhania <shivrajsinghania766@gmail.com>
2026-07-17 16:33:48 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,3 +3,4 @@ import os
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 SERPAPI_KEY = os.environ.get("SERPAPI_KEY")
+GEMINI_MODEL_CHAT = os.environ.get("GEMINI_MODEL_CHAT")
